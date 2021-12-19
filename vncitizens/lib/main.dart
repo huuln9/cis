4941263eb61552640df4a9d0629ec4556c518c52
Lý Ngọc Hữu <huuln9@gmail.com>
2021-12-19 19:09:44 +0700

    change template

--- vncitizens/lib/main.dart
+++ vncitizens/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:vncitizens/menu_ui.dart';
 import 'package:vncitizens_home/vncitizens_home.dart';
 
 void main() {
@@ -9,7 +8,6 @@ void main() {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       getPages: [
-        GetPage(name: '/te', page: () => const MenuUI()),
         GetPage(name: '/', page: () => const Splash()),
         GetPage(name: '/home', page: () => const Home()),
       ],
