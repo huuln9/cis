2e0b2fe6027fc6c8d9c25e11ff9f213b76686e71
huuln9 <huuln9@gmail.com>
2023-04-01 09:50:34 +0700

    ud ui

--- mvc/views/pages/mv_list_by_t.php
+++ mvc/views/pages/mv_list_by_t.php
@@ -39,7 +39,6 @@ $tags = json_decode($data['tags']);
                                         <th>Ảnh bìa</th>
                                         <th>Link</th>
                                         <th>Diễn viên</th>
-                                        <th>Thể loại</th>
                                         <th>Thao tác</th>
                                     </tr>
                                 </thead>
@@ -74,20 +73,6 @@ $tags = json_decode($data['tags']);
                                                     }
                                                 }
                                             }
-                                            ?>
-                                        </td>
-                                        <td>
-                                        
-                                            <?php
-                                            foreach ($mvTags as $mvTag) {
-                                                if ($mvTag->{'mvId'} == $row->{'id'}) {
-                                                    foreach ($tags as $tag) {
-                                                        if ($tag->{'id'} == $mvTag->{'tagId'}) {
-                                                            echo "<a href='" . $appRootURL . "/tag/list" . "'><span class='badge badge-primary'>" . $tag->{'name'} . "</span></a>". " ";
-                                                        }
-                                                    }
-                                                }
-                                            }    
                                             ?>
                                         </td>
                                         <td>
