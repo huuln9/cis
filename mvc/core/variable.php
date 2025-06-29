c4302c7b3a4079ee72e406b72c69fe91c2ce2f0d
huuln9 <huuln9@gmail.com>
2025-06-29 15:14:52 +0700

    ud

--- mvc/core/variable.php
+++ mvc/core/variable.php
@@ -2,6 +2,6 @@
 $appRootURL = "http://localhost/github/cis"; // controller
 $appRootDir = "C:/xampp/htdocs/github/cis"; // controller
 
-$actressPageCount = 6;
-$mvPageCount = 19;
+$actressPageCount = 7;
+$mvPageCount = 20;
 ?>
